bfabd4cf63b693f22434b4d2afda6c5095a979b8
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-06-05 05:33:21 -0400

    Add bubble sort with no function calls.

---
+++ sort_no_func_call.py
@@ -0,0 +1,10 @@
+arr = [5, 1, 4, 2, 8]
+n = len(arr)
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if arr[j] > arr[j + 1]:
+            temp = arr[j]
+            arr[j] = arr[j + 1]
+            arr[j + 1] = temp
+
+print(arr)
