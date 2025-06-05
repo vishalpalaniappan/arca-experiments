05077ae15172f5366186f89a4d426142d63726af
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-06-05 05:33:29 -0400

    Add bubble sort with function calls.

---
+++ sort_w_func_call.py
@@ -0,0 +1,17 @@
+import sys
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+
+def main():
+    arr = [5, 1, 4, 2, 8]
+    bubbleSort(arr)
+
+if __name__ == "__main__":
+    sys.exit(main())
